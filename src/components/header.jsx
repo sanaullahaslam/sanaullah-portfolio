2a63cbcbd3930b8f68bfb332f0005855b00d69d9
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-16 23:44:31 +0500

    header updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
+  const headerStyle = {
+    fontWeight: 'bold',
+    fontSize: '24px',
+    color: '#333', // Adjust the color as needed
+  };
+
   return (
-    <header>
-      <h1>Sanaullah's Portfolio</h1>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand style={headerStyle}>SanaUllah</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
