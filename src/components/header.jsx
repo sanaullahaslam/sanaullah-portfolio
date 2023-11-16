ddc930d7c6774bcdeccfa60d29060a0f874bf943
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-16 23:18:17 +0500

    3

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,7 @@ const Header = () => {
     <header>
       <h1>Your Website Title</h1>
       <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
+        <ul><li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
