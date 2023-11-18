4707e29dfdf4ca1ce71116fd030976639a12e038
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-18 15:26:18 +0500

    github proflie update

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
+    github: "https://github.com/sanaullahaslam",
     resume:
       "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
