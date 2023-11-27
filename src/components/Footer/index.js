4e2ff556701bcf1aebbd1cd0311d14ec602b932c
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-27 14:35:01 +0500

    changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Sanaullah Aslam</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Rishav Chanda. All rights reserved.
+          &copy; 2023 Sanaullah Aslam. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
