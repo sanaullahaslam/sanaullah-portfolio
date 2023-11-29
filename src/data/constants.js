26829899fc62988dd5fff7341e11903576bc4397
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-11-29 20:01:36 +0500

    changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -222,8 +222,8 @@ export const Bio = {
         "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
       tags: ["React Js", "Firebase", "Firestore", "Node JS"],
       category: "web app",
-      github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-      webapp: "https://whatsapp-clone-rishav.web.app",
+    //  github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
+     // webapp: "https://whatsapp-clone-rishav.web.app",
     },
     {
       id: 5,
@@ -235,8 +235,8 @@ export const Bio = {
         "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
       tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
       category: "web app",
-      github: "https://github.com/rishavchanda/Todo-Web-App",
-      webapp: "https://rishav-react-todo.netlify.app/",
+    //  github: "https://github.com/rishavchanda/Todo-Web-App",
+     // webapp: "https://rishav-react-todo.netlify.app/",
     },
     {
       id: 6,
@@ -248,8 +248,8 @@ export const Bio = {
         "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
       tags: ["React Js", "API", "Axios", "Node JS"],
       category: "web app",
-      github: "https://github.com/rishavchanda/Breaking-Bad",
-      webapp: "https://breaking-bad-webapp.netlify.app",
+    //  github: "https://github.com/rishavchanda/Breaking-Bad",
+     // webapp: "https://breaking-bad-webapp.netlify.app",
     },
     {
       id: 7,
@@ -261,8 +261,8 @@ export const Bio = {
         "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
       tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
       category: "android app",
-      github: "https://github.com/rishavchanda/Quiz-Earn",
-      webapp: "https://github.com/rishavchanda/Quiz-Earn",
+   //   github: "https://github.com/rishavchanda/Quiz-Earn",
+    //  webapp: "https://github.com/rishavchanda/Quiz-Earn",
     },
     {
       id: 8,
